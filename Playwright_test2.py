2a6566920f563708fd031c0adf2090788a9fe2d5
webturist <webturist@gmail.com>
2023-05-24 02:36:43 +0300

    update Playwright_test2

--- Playwright_test2.py
+++ Playwright_test2.py
@@ -1,3 +1,4 @@
+import sys
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
@@ -18,10 +19,19 @@ def run(playwright: Playwright) -> None:
     page.get_by_label("Email:").press("Tab")
     page.get_by_label("Password:").fill("pass")
     page.get_by_role("button", name="Submit").click()
+    sys.stderr.write('New User has been created\n')
+
+
+    try:
+        assert page.query_selector('text="You have successfully registered!"') is not None
+        sys.stderr.write('Test passed: registration success message displayed correctly\n')
+    except:
+        sys.stderr.write(f'Error: registration success message not displayed correctly\n')
+
+
 
     if page.get_by_text('Username already exists. Please choose a different one.'):
         page.get_by_role("link", name="Login").click()
-
 
     # Вхід в систему
     page.get_by_label("Username:").click()
@@ -29,9 +39,14 @@ def run(playwright: Playwright) -> None:
     page.get_by_label("Username:").press("Tab")
     page.get_by_label("Password:").fill("pass")
     page.get_by_role("button", name="Submit").click()
+    sys.stderr.write('New user has logged in\n')
 
     # Перевірка, чи відображається індексна сторінка
-    assert page.title() == 'Welcome to FlaskBlog'
+    try:
+        assert page.title() == 'Welcome to FlaskBlog'
+        sys.stderr.write('Test passed: the title page is displayed correctly\n')
+    except:
+        sys.stderr.write(f'Error: the title page not displayed correctly\n')
 
     # Створення нового запису
     page.locator("#navbarNav").get_by_role("link", name="New Post").click()
@@ -40,12 +55,26 @@ def run(playwright: Playwright) -> None:
     page.get_by_placeholder("Post title").press("Tab")
     page.get_by_placeholder("Post content").fill("text post")
     page.get_by_role("button", name="Submit").click()
+    sys.stderr.write('New Post has been created\n')
+    page.locator("div").filter(
+        has_text="Welcome to FlaskBlog").get_by_role("link", name="New Post").first.click()
 
     # Перевірка, чи відображається сторінка зі створеним записом
+    try:
+        assert page.title() == 'New Post by test'
+        sys.stderr.write('Test passed: New Post is displayed correctly\n')
+    except:
+        sys.stderr.write(f'Error: New Post not displayed correctly\n')
 
-    assert page.get_by_text('New Post')
+    # Видалення цього запису
+    # page.locator("div").filter(has_text="Welcome to FlaskBlog").get_by_role("link", name="New Post").click()
+    # page.get_by_role("link", name="Edit").click()
+    # page.get_by_role("button", name="Delete Post").click()
+    # page.once("dialog", lambda dialog: dialog.dismiss())
+    # page.get_by_text("\"New post\" was successfully deleted!").click()
 
     # Редагування даних користувача
+    page.get_by_role("link", name="FlaskBlog").click()
     page.get_by_role("link", name="Profile").click()
     page.get_by_role("link", name="Edit Profile").click()
     page.get_by_label("Username:").click()
@@ -53,6 +82,7 @@ def run(playwright: Playwright) -> None:
     page.get_by_label("Email:").click()
     page.get_by_label("Email:").fill("test@com.ua")
     page.get_by_role("button", name="Submit").click()
+    sys.stderr.write('User has changed the data\n')
 
     # Вихід з системи
     page.get_by_role("link", name="Logout").click()
@@ -60,7 +90,7 @@ def run(playwright: Playwright) -> None:
     # Закриття браузера
     context.close()
     browser.close()
-    print('Test passed: All assertions succeeded.')
+
 
 with sync_playwright() as playwright:
     run(playwright)
